a67396b0cc5c57e8a70f40bc82320b9fa2369d38
Maja Desic <desicmaja@yahoo.com>
2018-10-11 21:50:14 +0200

    Racunanje god,mesec  i dan

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 
-const pasus1= document.getElementById("pera")
+const pasus1= document.getElementById("pera");
 
-function izracunajDane() {
-    satiEkspert=10000
-    satiPocetnik=2000
-    brojSatiDnevno=document.getElementById("unos").value
-    brojDana = satiPocetnik / brojSatiDnevno
-    //console.log(brojDana)
-    console.log("Broj dana ucenja do pocetnika je:",brojDana)
-    brojGodinaDoPocetnika=brojDana / 365
-    console.log("Broj godina ucenja do pocetnika je:",brojGodinaDoPocetnika.toFixed(2))
-    pasus1.innerText="Broj dana ...."
-    satiPraksa=(satiEkspert-satiPocetnik)/8
-    ukupanBrojDana = brojDana + satiPraksa
-    ukupanBrojGodinaDoEksperta=ukupanBrojDana / 365
-    console.log("Broj dana ucenja do eksperta je:",ukupanBrojDana)
-    console.log("Broj godina ucenja do eksperta je:",ukupanBrojGodinaDoEksperta.toFixed(2))
-    
+function getBrojSatiDnevno() {
+    return document.getElementById("unos").value;
 }
 
-const dugme = document.getElementById("klikni")
-console.log(dugme)
+function izracunajDane() {
+    satiEkspert=10000;
+    satiPocetnik=2000;
+    brojSatiDnevno=getBrojSatiDnevno();
+
+    brojDanaZaPocetnika = satiPocetnik / brojSatiDnevno;
+    console.log("Broj dana ucenja do pocetnika je:",brojDanaZaPocetnika);
+
+    brojGodinaDoPocetnika=brojDanaZaPocetnika / 365;
+    console.log("Broj godina ucenja do pocetnika je:",brojGodinaDoPocetnika.toFixed(2));
+
+    pasus1.innerText="Broj dana ....";
+
+    daniPrakseDaPocetnikPostaneExpert=(satiEkspert-satiPocetnik)/8;
+    ukupanBrojDanaDoEksperta = brojDanaZaPocetnika + daniPrakseDaPocetnikPostaneExpert;
+    ukupanBrojGodinaDoEksperta=ukupanBrojDanaDoEksperta / 365;
+    console.log("Broj dana ucenja do eksperta je:",ukupanBrojDanaDoEksperta);
+    console.log("Broj godina ucenja do eksperta je:",ukupanBrojGodinaDoEksperta.toFixed(2));
+
+    ////////////////////////////////////////////////
+    var celihGodina = Math.floor(ukupanBrojDanaDoEksperta/365);
+    var ostatakDanaGodina = ukupanBrojDanaDoEksperta - (celihGodina * 365);
+    var brojMeseci = Math.floor(ostatakDanaGodina / 30);
+    var dani = ostatakDanaGodina - (brojMeseci * 30);
+    var message = `Potrebno je ${celihGodina} godina, ${brojMeseci} meseci i ${dani} dana da se dodje do eksperta`; // Template string
+    // var message = 'Potrebno je ' + celihGodina + ' godina, ' + brojMeseci + '' ;
+    console.log(message);
+}
+
+const dugme = document.getElementById("klikni");
+console.log(dugme);
 //dugme.onclick = izracunajDane
-dugme.addEventListener("click", izracunajDane)
+dugme.addEventListener("click", izracunajDane);
